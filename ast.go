cb4401567dc87a6e04e25d1aa85860d154bb82cf
akristianlopez <30592645+akristianlopez@users.noreply.github.com>
2025-11-26 19:22:07 +0100

    Update of 26-11-2025
    Update of 26-11-2025

--- ast.go
+++ ast.go
@@ -1,261 +1,276 @@
 package ast
 
-import (
-	"fmt"
-)
-
-// --- 1. Interfaces de Base de l'AST ---
-
-// Node est l'interface racine pour tous les nœuds de l'AST.
 type Node interface {
-	String() string // Pour l'affichage debug de l'AST
+	Pos() int
+	End() int
+	// String()
 }
 
-// Statement est une instruction (par exemple, un `if`, une boucle, une affectation).
+type Program struct {
+	Knowledge    StringLiteral
+	Declarations *BlockDclStatement //Statement
+	Start        *StartStatement
+}
+
+type StartStatement struct {
+	ObjectName StringLiteral
+	Statements []Statement
+}
+
+type LetFuncTypeDcl interface {
+	Node
+	statementNode()
+}
+
 type Statement interface {
 	Node
-	statementNode() // Méthode de "marquage" pour distinguer les Statements
+	statementNode()
 }
 
-// Expression est une expression (par exemple, un nombre, une variable, un appel de fonction).
 type Expression interface {
 	Node
-	expressionNode() // Méthode de "marquage" pour distinguer les Expressions
+	expressionNode()
 }
 
-// --- 2. Implémentations concrètes des nœuds de l'AST ---
-
-// Programme représente le nœud racine de tout le code source.
-type Action struct {
-	Name       string
-	Statements []Statement
-}
-
-func (p *Action) String() string {
-	var s string
-	for _, stmt := range p.Statements {
-		s += stmt.String() + "\n"
-	}
-	return "\t\t" + p.Name + "\r\n" + s
-}
-
-// --- 2.1 Nœuds d'Instructions (Statements) ---
-
-// ExpressionStatement est une instruction qui est juste une expression (ex: "5 + x;")
-type ExpressionStatement struct {
-	Expression Expression
-}
-
-func (es *ExpressionStatement) statementNode() {}
-func (es *ExpressionStatement) String() string { return es.Expression.String() }
-
-// BlockStatement représente un bloc d'instructions (souvent entre accolades {}).
-type BlockStatement struct {
-	Statements []Statement
-}
-
-func (bs *BlockStatement) statementNode() {}
-func (bs *BlockStatement) String() string {
-	var s string
-	for _, stmt := range bs.Statements {
-		s += stmt.String() + "; " // Simplifié pour l'affichage
-	}
-	// return "{ " + s + " }"
-	return s
-}
-
-// IfStatement représente une structure conditionnelle 'if'.
-type IfStatement struct {
-	Condition   Expression
-	Consequence *BlockStatement // Bloc d'instructions si la condition est vraie
-	Alternative *BlockStatement // Bloc d'instructions si la condition est fausse (optionnel)
-}
-
-func (is *IfStatement) statementNode() {}
-func (is *IfStatement) String() string {
-	str := fmt.Sprintf("IF (%s) THEN %s", is.Condition.String(), is.Consequence.String())
-	if is.Alternative != nil {
-		str += fmt.Sprintf(" ELSE %s", is.Alternative.String())
-	}
-	return str + "END IF"
-}
-
-// WhileStatement représente une boucle 'while'.
-type WhileStatement struct {
-	Condition Expression
-	Body      *BlockStatement
-}
-
-func (ws *WhileStatement) statementNode() {}
-func (ws *WhileStatement) String() string {
-	return fmt.Sprintf("WHILE (%s) DO %s END WHILE", ws.Condition.String(), ws.Body.String())
-}
-
-// ForStatement représente une boucle 'for'.
-type ForStatement struct {
-	InitStatement Statement
-	Condition     Expression
-	Body          *BlockStatement
-	LastStatement Statement
-}
-
-func (fs *ForStatement) statementNode() {}
-func (fs *ForStatement) String() string {
-	return fmt.Sprintf("FOR %s; %s; %s DO %s END FOR",
-		fs.InitStatement.String(), fs.Condition.String(),
-		fs.Body.String(), fs.LastStatement.String())
-}
-
-// ForStatement représente une boucle 'for each'.
-type ForEachStatement struct {
-	variable  *Identifier
-	Condition Expression
-	Body      *BlockStatement
-}
-
-func (fes *ForEachStatement) statementNode() {}
-func (fes *ForEachStatement) String() string {
-	return fmt.Sprintf("FOR EACH %s IN (%s) DO %s END FOR",
-		fes.variable.String(), fes.Condition.String(),
-		fes.Body.String())
-}
-
-// FunctionDeclaration représente la déclaration d'une fonction.
-type FunctionDeclaration struct {
+// Déclarations
+type FunctionDecl struct {
+	StartPos   int
 	Name       *Identifier
-	Parameters []*Identifier
+	Parameters []*Parameter
+	ReturnType Type
 	Body       *BlockStatement
 }
 
-func (fd *FunctionDeclaration) statementNode() {}
-func (fd *FunctionDeclaration) String() string {
-	params := make([]string, len(fd.Parameters))
-	for i, p := range fd.Parameters {
-		params[i] = p.String() + " " + p.Type.String()
-	}
-	return fmt.Sprintf("FUNCTION %s (%s) %s BEGIN %s END FUNCTION", fd.Name.Value, join(params, ", "), fd.Name.Type.String(), fd.Body.String())
+type Parameter struct {
+	Name *Identifier
+	Type Type
 }
 
-// ReturnStatement représente l'instruction 'return'.
+// Records
+type RecordDecl struct {
+	StartPos   int
+	Name       *Identifier
+	Parameters []*Parameter
+}
+
+// Types
+type Type interface {
+	Node
+	typeNode()
+}
+
+type BasicType struct {
+	StartPos int
+	Name     string // "integer", "float", "string", "boolean","time", "date"
+}
+
+type ArrayType struct {
+	StartPos    int
+	ElementType Type
+}
+
+// Statements
+type BlockStatement struct {
+	StartPos   int
+	Statements []Statement
+}
+
+// Statements
+type BlockDclStatement struct {
+	StartPos   int
+	Statements []VariableDecl
+}
+
+type IfStatement struct {
+	StartPos  int
+	Condition Expression
+	Then      Statement
+	Else      Statement
+}
+
+type SelectStatement struct {
+	StartPos   int
+	Condition  Expression
+	Statements []CaseStatement
+}
+
+type CaseStatement struct {
+	StartPos   int
+	Expr       Expression
+	Statements []Statement
+}
+
+type WhileStatement struct {
+	StartPos  int
+	Condition Expression
+	Body      Statement
+}
+
+type ForStatement struct {
+	StartPos  int
+	Init      Statement
+	Condition Expression
+	Update    Statement
+	Body      Statement
+}
+
+type ForEachStatement struct {
+	StartPos int
+	Variable *Identifier
+	Iterator Expression
+	Body     Statement
+}
+
+type TypeArgs struct {
+	Name  string
+	Value IntegerLiteral
+}
+
+type VariableDecl struct {
+	StartPos  int
+	Name      *Identifier
+	Type      Type
+	Arguments []TypeArgs
+	Value     Expression
+}
+
 type ReturnStatement struct {
-	ReturnValue Expression
+	StartPos int
+	Value    Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-func (rs *ReturnStatement) String() string {
-	return fmt.Sprintf("RETURN %s", rs.ReturnValue.String())
+type ExpressionStatement struct {
+	StartPos   int
+	Expression Expression
 }
 
-// --- 2.2 Nœuds d'Expressions (Expressions) ---
-
-// Identifier représente un nom (variable, nom de fonction).
+// Expressions
 type Identifier struct {
-	Value string
-	Type  *Type_name
+	StartPos int
+	Value    string
 }
 
-func (id *Identifier) expressionNode() {}
-func (id *Identifier) String() string {
-	// return strconv.FormatInt(il.Value, 10)
-	return id.Value
+type IntegerLiteral struct {
+	StartPos int
+	Value    int64
 }
 
-// Type_name représente un type de variable ou de valeur de retour
-// d'une fonction (Number, Boolean, String, Date, Time, ou un record ).
-type Type_name struct {
-	Value  string         //Nom du type
-	Intval *NumberLiteral //Nombre de chiffres de la partie entiere
-	Decval *NumberLiteral //Nombre de chiffres de la partie decimale
-	MinVal *NumberLiteral //Valeur minimale
-	MaxVal *NumberLiteral //Valeur maximale
+type FloatLiteral struct {
+	StartPos int
+	Value    float64
 }
 
-func (tn *Type_name) expressionNode() {}
-func (tn *Type_name) String() string {
-	str := tn.Value
-	bl := false
-	if len(tn.Intval.Value) > 0 {
-		str += " (" + tn.Intval.Value
-		if len(tn.Decval.Value) > 0 {
-			str += ", " + tn.Decval.Value
-		}
-		bl = true
-	}
-	if len(tn.MinVal.Value) > 0 {
-		switch {
-		case bl:
-			str += ", " + tn.MinVal.Value
-			if len(tn.MaxVal.Value) > 0 {
-				str += ": " + tn.MaxVal.Value
-			}
-		default:
-			bl = true
-			str += " (" + tn.MinVal.Value
-			if len(tn.MaxVal.Value) > 0 {
-				str += ": " + tn.MaxVal.Value
-			}
-		}
-	}
-	if bl {
-		str += ")"
-	}
-	return str
+type StringLiteral struct {
+	StartPos int
+	Value    string
 }
 
-// IntegerLiteral représente un nombre entier.
-type NumberLiteral struct {
-	Value string
-}
-
-func (il *NumberLiteral) expressionNode() {}
-func (il *NumberLiteral) String() string {
-	// return strconv.FormatInt(il.Value, 10)
-	return il.Value
-}
-
-// BooleanLiteral représente un booléen (true/false).
 type BooleanLiteral struct {
-	Value string
+	StartPos int
+	Value    bool
 }
 
-func (bl *BooleanLiteral) expressionNode() {}
-func (bl *BooleanLiteral) String() string  { return bl.Value /*strconv.FormatBool(bl.Value)*/ }
-
-// InfixExpression représente une opération binaire (ex: 5 + 3, x > y).
-type InfixExpression struct {
+type BinaryExpression struct {
+	StartPos int
 	Left     Expression
 	Operator string
 	Right    Expression
 }
 
-func (ie *InfixExpression) expressionNode() {}
-func (ie *InfixExpression) String() string {
-	return fmt.Sprintf("(%s %s %s)", ie.Left.String(), ie.Operator, ie.Right.String())
-}
-
-// CallExpression représente un appel de fonction.
 type CallExpression struct {
-	Function  Expression // Peut être un Identifier ou une autre Expression (ex: une fonction anonyme)
+	StartPos  int
+	Function  Expression
 	Arguments []Expression
 }
 
-func (ce *CallExpression) expressionNode() {}
-func (ce *CallExpression) String() string {
-	args := make([]string, len(ce.Arguments))
-	for i, a := range ce.Arguments {
-		args[i] = a.String()
-	}
-	return fmt.Sprintf("%s(%s)", ce.Function.String(), join(args, ", "))
+type ArrayLiteral struct {
+	StartPos int
+	Elements []Expression
 }
 
-// --- Fonctions utilitaires ---
-func join(s []string, sep string) string {
-	if len(s) == 0 {
-		return ""
+// Implémentations des méthodes Node
+func (p *Program) Pos() int { return 0 }
+func (p *Program) End() int { return 0 }
+
+// func (b BasicType) typeNode() {}
+// func (b BasicType) End() int  { return b.StartPos }
+// func (b BasicType) Pos() int  { return b.StartPos }
+
+func (f *FunctionDecl) Pos() int       { return f.StartPos }
+func (f *FunctionDecl) End() int       { return f.Body.End() }
+func (f *FunctionDecl) statementNode() {}
+
+func (b *BlockStatement) Pos() int { return b.StartPos }
+func (b *BlockStatement) End() int {
+	if len(b.Statements) > 0 {
+		return b.Statements[len(b.Statements)-1].End()
 	}
-	res := s[0]
-	for i := 1; i < len(s); i++ {
-		res += sep + s[i]
-	}
-	return res
+	return b.StartPos
 }
+func (b *BlockStatement) statementNode() {}
+
+func (i *IfStatement) Pos() int { return i.StartPos }
+func (i *IfStatement) End() int {
+	end := i.Then.End()
+	if i.Else != nil {
+		end = i.Else.End()
+	}
+	return end
+}
+func (i *IfStatement) statementNode() {}
+
+func (w *WhileStatement) Pos() int       { return w.StartPos }
+func (w *WhileStatement) End() int       { return w.Body.End() }
+func (w *WhileStatement) statementNode() {}
+
+func (f *ForStatement) Pos() int       { return f.StartPos }
+func (f *ForStatement) End() int       { return f.Body.End() }
+func (f *ForStatement) statementNode() {}
+
+func (fe *ForEachStatement) Pos() int       { return fe.StartPos }
+func (fe *ForEachStatement) End() int       { return fe.Body.End() }
+func (fe *ForEachStatement) statementNode() {}
+
+func (v *VariableDecl) Pos() int       { return v.StartPos }
+func (v *VariableDecl) End() int       { return v.Value.End() }
+func (v *VariableDecl) statementNode() {}
+
+func (r *ReturnStatement) Pos() int       { return r.StartPos }
+func (r *ReturnStatement) End() int       { return r.Value.End() }
+func (r *ReturnStatement) statementNode() {}
+
+func (e *ExpressionStatement) Pos() int       { return e.StartPos }
+func (e *ExpressionStatement) End() int       { return e.Expression.End() }
+func (e *ExpressionStatement) statementNode() {}
+
+func (i *Identifier) Pos() int        { return i.StartPos }
+func (i *Identifier) End() int        { return i.StartPos + len(i.Value) }
+func (i *Identifier) expressionNode() {}
+
+func (il *IntegerLiteral) Pos() int        { return il.StartPos }
+func (il *IntegerLiteral) End() int        { return il.StartPos }
+func (il *IntegerLiteral) expressionNode() {}
+
+func (fl *FloatLiteral) Pos() int        { return fl.StartPos }
+func (fl *FloatLiteral) End() int        { return fl.StartPos }
+func (fl *FloatLiteral) expressionNode() {}
+
+func (sl *StringLiteral) Pos() int        { return sl.StartPos }
+func (sl *StringLiteral) End() int        { return sl.StartPos + len(sl.Value) + 2 }
+func (sl *StringLiteral) expressionNode() {}
+
+func (bl *BooleanLiteral) Pos() int        { return bl.StartPos }
+func (bl *BooleanLiteral) End() int        { return bl.StartPos }
+func (bl *BooleanLiteral) expressionNode() {}
+
+func (b *BinaryExpression) Pos() int        { return b.Left.Pos() }
+func (b *BinaryExpression) End() int        { return b.Right.End() }
+func (b *BinaryExpression) expressionNode() {}
+
+func (c *CallExpression) Pos() int        { return c.Function.Pos() }
+func (c *CallExpression) End() int        { return c.Function.End() }
+func (c *CallExpression) expressionNode() {}
+
+func (a *ArrayLiteral) Pos() int        { return a.StartPos }
+func (a *ArrayLiteral) End() int        { return a.StartPos }
+func (a *ArrayLiteral) expressionNode() {}
